4bce7abbb26e141f147949a53abae67075eed417
Jordan Yeo <73056451+jordanyeo2001@users.noreply.github.com>
2022-01-31 12:05:49 +0800

    test scene

---
+++ Assets/test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("works");
+        Debug.Log(collision.relativeVelocity.magnitude);
+        if (collision.relativeVelocity.magnitude > 2)
+        {
+            
+            
+        }
+    }
+}
