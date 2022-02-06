72edd1e8259c6b9060bacb38200f6a7dd2fef22a
Jordan Yeo <73056451+jordanyeo2001@users.noreply.github.com>
2022-02-06 17:34:58 +0800

    Add Grind & CheckContents Interaction

---
+++ Assets/CheckContents.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckContents : MonoBehaviour
+{
+    private int contentCount;
+    public GameObject[] contentArray;
+    private bool contentChecked;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "BrownSugar")
+        {
+            Destroy(other.gameObject);
+            contentArray[0].SetActive(true);
+            contentCount++;
+        }
+        else if (other.gameObject.tag == "BlackSesameSeeds")
+        {
+            Destroy(other.gameObject);
+            contentArray[1].SetActive(true);
+            contentCount++;
+        }
+
+        if (contentCount == 2)
+        {
+            if (!contentChecked)
+            {
+                for (int i = contentCount; i > -1; i--)
+                {
+                    contentArray[i].SetActive(false);
+                }
+
+                contentArray[contentCount].SetActive(true);
+                contentChecked = true;
+            }
+            
+        }
+    }
+
+}
---
+++ Assets/GrindSequence.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrindSequence : MonoBehaviour
+{
+    private int grindCount;
+    public GameObject[] grindArray;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.relativeVelocity.magnitude);
+        if(collision.gameObject.tag == "Mortal")
+        {
+            if (collision.relativeVelocity.magnitude > 2 && grindCount < 3)
+            {
+                grindCount++;
+                Debug.Log("GrindCount: " + grindCount);
+            }
+
+            if(grindCount != 0)
+            {
+                grindArray[grindCount - 1].SetActive(false);
+            }
+            
+
+            if (grindCount == 1)
+            {
+                grindArray[grindCount].SetActive(true);
+            }
+            else if(grindCount == 2)
+            {
+                grindArray[grindCount].SetActive(true);
+            }
+            else if(grindCount == 3)
+            {
+                grindArray[grindCount].SetActive(true);
+            }
+        }
+        
+    }
+}
