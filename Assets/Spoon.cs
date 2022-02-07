a892d3f40cace40ccc9911b3ada80ce7c6a5b6f2
Jordan Yeo <73056451+jordanyeo2001@users.noreply.github.com>
2022-02-07 13:18:34 +0800

    Add Scooping interaction

---
+++ Assets/Spoon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spoon : MonoBehaviour
+{
+    public Transform spawnPosition;
+    public GameObject prefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Paste")
+        {
+            GameObject mySpoon =  Instantiate(prefab, spawnPosition.position, Quaternion.identity);
+            mySpoon.transform.parent = transform;
+        }
+
+        else if(other.gameObject.tag == "Plate")
+        {
+            foreach (Transform obj in transform)
+            {
+                if (obj.tag == "RoundDough")
+                {
+                    transform.parent = null;
+                }
+            }
+        }
+    }
+
+}
