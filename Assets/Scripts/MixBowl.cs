bc0dc8508a1beee877c5768970106126a197252c
Ng Hui Ling <68372136+huiling3o3@users.noreply.github.com>
2022-02-10 21:36:07 +0800

    interaction script done (some)

--- Assets/Scripts/MixBowl.cs
+++ Assets/Scripts/MixBowl.cs
@@ -10,7 +10,10 @@ public class MixBowl : MonoBehaviour
     //reference the stiring script
     public Stiring Stiring;
 
+    [Header("Check ingredient")]
+    [SerializeField]
     private bool waterInside;
+    [SerializeField]
     private bool flourInside;
 
     private void Start()
@@ -23,17 +26,20 @@ public class MixBowl : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log(other.gameObject.name + " is collided");
         //Check if the content needed is the correct item,
         //if yes, destroy the collider object && set it to appear on the mixing bowl
         if (other.gameObject.name == contentName[0])
         {
             Destroy(other.gameObject);
-            contentArray[0].SetActive(true);           
+            contentArray[0].SetActive(true);
+            waterInside = true;
         }
         else if (other.gameObject.name == contentName[1])
         {
             Destroy(other.gameObject);
             contentArray[1].SetActive(true);
+            flourInside = true;
         }
     }
 
---
+++ Assets/Scripts/Socket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Socket : MonoBehaviour
+{
+    public Transform target;
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(other.gameObject.name + " is collided");
+        if (other.gameObject.name == "kneadDough")
+        {
+            //set the object position to the target position
+            other.gameObject.GetComponent<Rigidbody>().MovePosition(target.position);
+            //other.gameObject.transform.position = target.position;
+        }
+    }
+}
