2ec57fb4530d726dc92c7e4ba5e042904550167c
Jordan Yeo <73056451+jordanyeo2001@users.noreply.github.com>
2022-02-11 14:00:27 +0800

    Add firebase Scripts and cutting interaction

---
+++ Assets/Scripts/FirebaseScripts/fsSerializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class fsSerializer
+{
+    internal void TryDeserialize(fsData userData, ref Dictionary<string, User> users)
+    {
+        throw new NotImplementedException();
+    }
+}
