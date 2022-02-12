78a2571975e951bb384a3ba87039356dd62b2753
Jordan Yeo <73056451+jordanyeo2001@users.noreply.github.com>
2022-02-12 17:21:10 +0800

    Add boiling interaction

---
+++ Assets/Scripts/Boiling.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boiling : MonoBehaviour
+{
+    public ParticleSystem[] boilContents;
+    private float timer;
+    private float duration = 10f;
+    private bool startBoil;
+    private void Update()
+    {
+        Debug.Log(timer);
+        if (startBoil == true) 
+        {
+            Debug.Log("START");
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                BoilWater();
+                startBoil = false;
+            }
+        }
+    }
+
+    public void StartTimer()
+    {
+        timer = duration;
+        startBoil = true;
+    }
+    private void BoilWater()
+    {
+        for (int i = 0; i < boilContents.Length; i++)
+        {
+            boilContents[i].Play();
+        }
+    }
+}
